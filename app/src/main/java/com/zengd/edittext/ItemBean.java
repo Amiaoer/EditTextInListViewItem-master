c86d0d60f305a31ae2b7469f2020b4ba32c73fbd
DUYANFENG\duyanfeng <271866937@qq.com>
2017-10-15 00:03:28 +0800

    Initial commit

---
+++ app/src/main/java/com/zengd/edittext/ItemBean.java
@@ -0,0 +1,16 @@
+package com.zengd.edittext;
+
+/**
+ * Created by melo on 2017/10/14 .
+ */
+public class ItemBean {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
